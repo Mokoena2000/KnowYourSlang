887f8d1e714a9a9f98744ceef6c9bdd01ea14afb
Moholeng <moholengmokoena2@gmail.com>
2025-06-09 12:06:28 +0200

    added 3rd question

--- Main/CapeSlang.py
+++ Main/CapeSlang.py
@@ -12,15 +12,16 @@ def Cape():
         points = 0
         
         Question1 = input("A friendly greeting, like Hey! or What's up ?: ")
-        Answer = "Awe"
+        Answer = "AWE"
 
-        if Question1.lower() == "awe":
+        if Question1.lower() == Answer.lower():
             points = points+5
             return (f"Yes the answer is {Answer} Heyy look at you... I knew you would get the answer Lets go.....\nyou still have {guesses} lives left and have scored {points} points")
         
         
         else:
-            return (f"Oops your answer {Question1} is not quiet right would you like to try again...\n coz its your first try we wont be deducting any lives ")
+            print(f"Oops your answer {Question1} is not quiet right would you like to try again...\n coz its your first try we wont be deducting any lives ")
+            return input("A friendly greeting, like Hey! or What's up ?: ")
         
         
     print(FirstQuestion())
@@ -30,15 +31,15 @@ def Cape():
         points = 0
 
         Question2 = input("A gattering where people get together and put some meat on the coals: ")
-        Answer = "Braai"
+        Answer = "BRAAI"
 
-        if  Question2.lower() == "braai":
+        if  Question2.lower() == Answer.lower():
             points = points+5
             return (f"Look at you doing your thing 2 in a row your answer {Answer} is correct you just scored another {points} points and have {guesses} lives left")
         
         else:
             guesses = guesses-1
-            return (f"Eishhhh not quiet right your answer {Answer} is not what we looking for...you can try again your lives are {guesses} we have deducted 1")
+            return (f"Eishhhh not quiet right your answer {Question2} is not what we looking for...you can try again your lives are {guesses} we have deducted 1")
             
         
 
@@ -46,14 +47,23 @@ def Cape():
 
     def ThirdQuestion():
         guesses = 7
+        Wrongguesses = 6
         points = 0
 
         Question3 = input("Greasy or junk food commonly eaten on a night out")
+        Answer = "VUIL DITE"
+
+        if Question3.lower() == Answer.lower().strip():
+            points = points+5
+            return (f"Your answer{Question3} is right how bout we enjoy a nice {Answer} together you scored yourself {points} points and a have {guesses} lives")
+
+        else:
+            Wrongguesses =  Wrongguesses-1
+            return (f"I guess someone wont be getting some extra points {points} points left and {Wrongguesses}  have deducted 1")           
         
+    print(ThirdQuestion())
 
 
-        
-         
 if __name__ == "__main__":
     Cape()
     
